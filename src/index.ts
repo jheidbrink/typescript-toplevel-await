f4692a23213ee490ed4d5e83a96c2cc5f384e991
Jan Heidbrink <jan.heidbrink@posteo.de>
2024-01-31 17:04:16 +0100

    Remove sayHelloWorld
    This was autogenerated by npm init

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
 import axios from 'axios';
 
-export function sayHelloWorld(world: string) {
-  return `Hello ${world}`;
-}
-
 const result = await axios.get("https://httpbin.org/get");
 console.log(result);
